e1e31cb0e8dc33a9c15dded24069ea1ee426b7e6
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-06-18 19:32:29 +0300

    Login page added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './pages/Home';
-import Navbar from './shared/Navbar';
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
+import Login from './pages/login/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
 
--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import peopleWorking from "../photos/people-working.jpg";
 import "./Home.css";
 
 export default function Home() {
     return (
         <div className="container">
             <div className="search-section">
-
             </div>
         </div>
     );
