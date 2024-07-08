abb14ebf7a91c78e6930c5830984e1e7f1320e69
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-07-08 17:53:39 +0300

    Job offer container styled

--- Client/src/components/jobOffer/JobOffer.jsx
+++ Client/src/components/jobOffer/JobOffer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./JobOffer.css";
 
 export default function JobOffer({ title, country, city, companyImgUrl, postedOn, isFullTime }) {
-    // Format the postedOn date
     const formattedDate = new Date(postedOn).toLocaleDateString();
 
     return (
@@ -11,39 +10,35 @@ export default function JobOffer({ title, country, city, companyImgUrl, postedOn
           <h2>{title}</h2>
           <div className="job-details">
             <div className="detail">
+              <img
+                src="https://img.icons8.com/?size=100&id=85149&format=png&color=000000"
+                alt="Location Icon"
+              />
               <p>
-                <img
-                  src="https://img.icons8.com/?size=100&id=85149&format=png&color=000000"
-                  alt="Location Icon"
-                />
                 {country}, {city}
               </p>
             </div>
             <div className="detail">
-              <p>
-                <img
-                  src="https://img.icons8.com/?size=100&id=3344&format=png&color=000000"
-                  alt="Calendar Icon"
-                />
-                {formattedDate}
-              </p>
+              <img
+                src="https://img.icons8.com/?size=100&id=3344&format=png&color=000000"
+                alt="Calendar Icon"
+              />
+              <p>{formattedDate}</p>
             </div>
             <div className="detail">
-              <p>
-                <img
-                  src={
-                    isFullTime
-                      ? "https://img.icons8.com/?size=100&id=109233&format=png&color=000000"
-                      : "https://img.icons8.com/?size=100&id=24275&format=png&color=000000"
-                  }
-                  alt={isFullTime ? "Full Time Icon" : "Part Time Icon"}
-                />
-                {isFullTime ? "Full Time" : "Part Time"}
-              </p>
+              <img
+                src={
+                  isFullTime
+                    ? "https://img.icons8.com/?size=100&id=109233&format=png&color=000000"
+                    : "https://img.icons8.com/?size=100&id=24275&format=png&color=000000"
+                }
+                alt={isFullTime ? "Full Time Icon" : "Part Time Icon"}
+              />
+              <p>{isFullTime ? "Full Time" : "Part Time"}</p>
             </div>
           </div>
         </div>
-        <img src={companyImgUrl} alt="Company Logo" />
+        <img className="company-logo" src={companyImgUrl} alt="Company Logo" />
       </div>
     );
 }
