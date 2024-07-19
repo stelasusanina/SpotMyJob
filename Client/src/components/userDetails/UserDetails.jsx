005fab90f6ec32cb11113700f81172d0c34bd379
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-07-19 18:18:07 +0300

    My profile page styled

--- Client/src/components/userDetails/UserDetails.jsx
+++ Client/src/components/userDetails/UserDetails.jsx
@@ -6,6 +6,7 @@ import "./UserDetails.css";
 export default function UserDetails() {
   const [details, setDetails] = useState({});
   const [myApplications, setMyApplications] = useState([]);
+  const [expandedApplications, setExpandedApplications] = useState([]); 
   const { identify } = useAuth();
 
   useEffect(() => {
@@ -15,7 +16,7 @@ export default function UserDetails() {
         const response = await axiosClient.get(
           `${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/details`,
           {
-            params: { userId }
+            params: { userId },
           }
         );
         setDetails(response.data);
@@ -30,42 +31,75 @@ export default function UserDetails() {
         const response = await axiosClient.get(
           `${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/jobApplications`,
           {
-            params: { userId }
+            params: { userId },
           }
         );
         setMyApplications(response.data);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
 
     fetchUserDetails();
     fetchApplications();
   }, [identify]);
 
   const removePhoto = () => {
-    axiosClient.post(`${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/removeProfilePhoto`, {
-      userId: details.id
-    })
-    .then(() => {
-      window.location.reload();
-    })
-    .catch(error => console.log(error));
-  };
-
-  const uploadPhoto = () => {
-    const profilePhotoUrl = prompt("Please enter the URL of your profile photo:");
-    console.log(profilePhotoUrl);
-    if (profilePhotoUrl) {
-      axiosClient.post(`${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/uploadProfilePhoto`, {
-        profilePhotoUrl
-      })
+    axiosClient
+      .post(
+        `${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/removeProfilePhoto`,
+        {
+          userId: details.id,
+        }
+      )
       .then(() => {
         window.location.reload();
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
+  };
+
+  const uploadPhoto = () => {
+    const profilePhotoUrl = prompt(
+      "Please enter the URL of your profile photo:"
+    );
+    console.log(profilePhotoUrl);
+    if (profilePhotoUrl) {
+      axiosClient
+        .post(
+          `${process.env.REACT_APP_API_BASE_URL}/auth/myProfile/uploadProfilePhoto`,
+          {
+            profilePhotoUrl,
+          }
+        )
+        .then(() => {
+          window.location.reload();
+        })
+        .catch((error) => console.log(error));
     }
   };
+
+  const toggleExpand = (jobOfferId) => {
+    setExpandedApplications((prevState) =>
+      prevState.includes(jobOfferId)
+        ? prevState.filter((id) => id !== jobOfferId)
+        : [...prevState, jobOfferId]
+    );
+  };
+
+  const getStatusClass = (status) => {
+    switch(status) {
+      case 'Applied':
+        return 'applied';
+      case 'In Progress':
+        return 'in-progress';
+      case 'Completed':
+        return 'completed';
+      case 'Rejected':
+        return 'rejected'; 
+      default:
+        return '';
+    }
+  }  
 
   return (
     <div className="user-details">
@@ -94,6 +128,7 @@ export default function UserDetails() {
       </div>
       {details && (
         <div className="user-details-info">
+          <h3>Personal information</h3>
           <p>{`${details.firstName} ${details.lastName}`}</p>
           <p>{details.phoneNumber}</p>
           <p>{details.email}</p>
@@ -101,15 +136,36 @@ export default function UserDetails() {
           <div className="my-applications">
             {myApplications &&
               myApplications.map((application) => (
-                <div className="application" key={application.id}>
-                  <p>{application.jobOfferTitle}</p>
-                  <span>See more</span>
-                  <div className="see-more">
-                    <p>{application.jobCompanyName}</p>
-                    <p>{application.uploadedFileName}</p>
-                    <p>{application.appliedOn}</p>
-                    <p className="status">{application.status}</p>
+                <div className="application" key={application.jobOfferId}>
+                  <div className="short-info">
+                    <p className="title">{application.jobOfferTitle}</p>
+                    <span onClick={() => toggleExpand(application.jobOfferId)}>
+                      {expandedApplications.includes(application.jobOfferId) ? (
+                        <i class="fa-solid fa-caret-up"></i>
+                      ) : (
+                        <i class="fa-solid fa-caret-down"></i>
+                      )}
+                    </span>
                   </div>
+                  {expandedApplications.includes(application.jobOfferId) && (
+                    <div className="see-more">
+                      <p>Company: {application.jobCompanyName}</p>
+                      <p>Uploaded file: {application.uploadedFileName}</p>
+                      <p>
+                        Applied on:{" "}
+                        {new Date(application.appliedOn).toLocaleString()}
+                      </p>
+                      <p>
+                        Status:{" "}
+                        <span
+                          className={`status-${getStatusClass(
+                            application.status
+                          )}`}>
+                          {application.status}
+                        </span>
+                      </p>{" "}
+                    </div>
+                  )}
                 </div>
               ))}
           </div>
