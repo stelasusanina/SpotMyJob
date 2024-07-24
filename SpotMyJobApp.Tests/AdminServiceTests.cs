c72828b10b9d142359ae5fff6a91ab0bed81a01a
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-07-24 18:07:22 +0300

    Corrected some tests

--- SpotMyJobApp.Tests/AdminServiceTests.cs
+++ SpotMyJobApp.Tests/AdminServiceTests.cs
@@ -37,7 +37,7 @@ namespace SpotMyJobApp.Tests
 			var model = new AddJobOfferDto
 			{
 				Title = "Software Developer",
-				CompanyName = "Tech Corp",
+				CompanyName = "Tech",
 				CompanyImgUrl = "http://example.com/logo.png",
 				CompanyDescription = "Leading tech company",
 				Country = "USA",
@@ -55,33 +55,36 @@ namespace SpotMyJobApp.Tests
 			var jobOffer = await context.JobOffers.Include(jo => jo.Sections).FirstOrDefaultAsync();
 			Assert.NotNull(jobOffer);
 			Assert.Equal("Software Developer", jobOffer.Title);
-			Assert.Equal("Tech Corp", jobOffer.CompanyName);
+			Assert.Equal("Tech", jobOffer.CompanyName);
 			Assert.Equal("USA", jobOffer.Country);
-			Assert.Single(jobOffer.Sections);
 		}
 
 		[Fact]
 		public async Task ChangeStatusOfApplicationAsync_ChangesStatus()
 		{
-			var userId = "testUserId";
-			var jobOfferId = 1;
+			var userId = "g543-985p-iu96-97-20l8";
+			var jobOfferId = 5;
+
 			var jobApplication = new JobApplication
 			{
 				JobOfferId = jobOfferId,
 				ApplicationUserId = userId,
-				Status = "Pending",
+				Status = "Applied",
 				UploadedFileName = "CV.pdf"
 			};
 
 			await context.JobsApplications.AddAsync(jobApplication);
 			await context.SaveChangesAsync();
 
-			await adminService.ChangeStatusOfApplicationAsync(userId, jobOfferId, "Accepted");
+			await adminService.ChangeStatusOfApplicationAsync(userId, jobOfferId, "Completed");
 
-			var updatedApplication = await context.JobsApplications.FirstOrDefaultAsync();
+			var updatedApplication = await context.JobsApplications
+				.FirstOrDefaultAsync(a => a.ApplicationUserId == userId && a.JobOfferId == jobOfferId);
+
 			Assert.NotNull(updatedApplication);
-			Assert.Equal("Accepted", updatedApplication.Status);
+			Assert.Equal("Completed", updatedApplication.Status);
 		}
+
 
 		[Fact]
 		public async Task DeleteJobOfferAsync_DeletesJobOffer()
